81c6d6c3fd781f3e4ae8ccea8812699a589b3ccd
Syunsuke Kasai <syunn2112@gmail.com>
2025-12-07 18:23:39 +0900

    [add]CodeQLでエラーをはくコードの追加

--- TestProject/Controllers/HomeController.cs
+++ TestProject/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using TestProject.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace TestProject.Controllers
 {
@@ -16,10 +18,33 @@ namespace TestProject.Controllers
             return View();
         }
 
+        // ? わざと危険なコード（SQLインジェクション）
+        public IActionResult Dangerous(string userInput)
+        {
+            string connectionString = "Server=localhost;Database=TestDb;Trusted_Connection=True;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // ?? ユーザー入力を直接 SQL に連結（超危険）
+                string sql = "SELECT * FROM Users WHERE Name = '" + userInput + "'";
+
+                SqlCommand command = new SqlCommand(sql, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                // 本来は何もしない（検出目的）
+                return Content("Executed");
+            }
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
